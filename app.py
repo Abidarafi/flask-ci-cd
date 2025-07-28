79f30fb3019ee99fb4466d6246408551bbe83b4f
Abidarafi <abidmuhammadarafi519@email.com>
2025-07-29 02:05:49 +0700

    Fix __name__ typo in app.py

--- app.py
+++ app.py
@@ -1,19 +1,7 @@
 from flask import Flask
-import psycopg2
-import os
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    # Koneksi PostgreSQL dari Railway
-    conn = psycopg2.connect(os.environ.get("DATABASE_URL"))
-    cur = conn.cursor()
-    cur.execute("SELECT NOW()")
-    result = cur.fetchone()
-    cur.close()
-    conn.close()
-    return f"Connected to DB! Current time: {result[0]}"
-
-if _name_ == "_main_":
-    app.run(debug=True)
+def home():
+    return "Halo dari Flask di Railway!"
