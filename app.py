20065afb63e6646e0fcba875e8cb591abdf7a4b5
Abidarafi <abidmuhammadarafi519@email.com>
2025-07-29 07:08:37 +0700

    Save local changes to app.py before merge

--- app.py
+++ app.py
@@ -3,19 +3,25 @@ from flask_sqlalchemy import SQLAlchemy
 import os
 
 app = Flask(__name__)
+
+# Konfigurasi SQLite Database
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+# Model Database
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100))
-    content = db.Column(db.Text)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.Text, nullable=False)
 
+# Halaman utama - Daftar semua post
 @app.route('/')
 def index():
     posts = Post.query.all()
     return render_template('index.html', posts=posts)
 
+# Buat post baru
 @app.route('/create', methods=['GET', 'POST'])
 def create():
     if request.method == 'POST':
@@ -27,6 +33,7 @@ def create():
         return redirect(url_for('index'))
     return render_template('create.html')
 
+# Edit post
 @app.route('/edit/<int:id>', methods=['GET', 'POST'])
 def edit(id):
     post = Post.query.get_or_404(id)
@@ -37,6 +44,7 @@ def edit(id):
         return redirect(url_for('index'))
     return render_template('edit.html', post=post)
 
+# Hapus post
 @app.route('/delete/<int:id>')
 def delete(id):
     post = Post.query.get_or_404(id)
@@ -44,8 +52,9 @@ def delete(id):
     db.session.commit()
     return redirect(url_for('index'))
 
+# Run server
 if __name__ == '__main__':
-    if not os.path.exists('blog.db'):
-        with app.app_context():
-            db.create_all()
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Untuk Railway / lokal
+    with app.app_context():
+        db.create_all()  # Buat tabel jika belum ada
+    app.run(debug=True, host='0.0.0.0', port=port)
